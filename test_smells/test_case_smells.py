bc71d590948ae2b67ba5e09321aef6d0402d58cb
Eric Tiano <etiano417@gmail.com>
2018-05-04 13:47:52 -0400

    Added General Fixture detection
     - Completed GeneralFixture
     - Created GeneralFixtureSetUpVisitor
     - Created GeneralFixtureTestMethodVisitor
     - Added GeneralFixture to test_case_rule_runner

--- test_smells/test_case_smells.py
+++ test_smells/test_case_smells.py
@@ -1,5 +1,7 @@
 from test_smells.test_smell import TestSmell
 from test_smells.smell_visitor import SmellVisitor
+import python_parser
+import ast
 
 class GeneralFixture(TestSmell):
     name = "General Fixture"
@@ -38,6 +40,75 @@ class DefaultTest(TestSmell):
         if(ast.name in self.default_case_names):
             return self.name
             
+    
+class GeneralFixture(TestSmell):
+
+    def __init__(self):
+        self.name = "General Fixture"
+        self.visitor_1 = GeneralFixtureSetUpVisitor()
+        self.visitor_2 = GeneralFixtureTestMethodVisitor()
+        
+    def test_for_smell(self, ast):
+        
+        self.visitor_1.visit(ast)
+        
+        #a list of attributes assigned in setUp
+        attributes = self.visitor_1.attribute_lst
+        
+        #ast needs to be passed as a tuple because get_test_asts is
+        #primarily for use by smell_detector
+        test_methods = python_parser.get_test_asts((ast,None))
+        
+        for test in test_methods:
+        
+            self.visitor_2.visit(test[0])
+            
+        #remove all attributes found being used from the list
+        attributes -= self.visitor_2.found_attributes   
+        
+        if(len(attributes) > 0):
+            return self.name
+        
+        
+class GeneralFixtureSetUpVisitor(SmellVisitor):
+    def __init__(self):
+        self.attribute_lst = set()
+        super(GeneralFixtureSetUpVisitor,self).__init__()
+
+    def visit_FunctionDef(self, node):
+    
+        setupMethodName = "setUp"
+        
+        if node.name == setupMethodName:
+
+            #check all the items in the setUp function
+            for body_item in ast.walk(node):
+                if isinstance(body_item, ast.Assign): 
+
+                    for target in body_item.targets:
+                        try:
+                            #ensure the assignment is to an attribute
+                            if(target.value.id == "self"): 
+                                self.attribute_lst.add(target.attr)
+                                
+                        except:
+                            pass
+                        
+        else:
+            pass
+        super().generic_visit(node)
+        
+        
+class GeneralFixtureTestMethodVisitor(SmellVisitor):
+
+    def __init__(self):
+        self.found_attributes = set()
+        super(GeneralFixtureTestMethodVisitor,self).__init__()
+    
+    def visit_Attribute(self, node):
+        if(node.value.id == "self"):
+            self.found_attributes.add(node.attr)
+
     
     
 def dummy_code_call(smell,test_case_ast):
